6617c6771afaab23ca14eea37a07264aab991ec8
Jessica Apps <japps156@gmail.com>
2024-01-26 22:04:24 +0000

    added tables to the nodes, next problem is adding code blocks

--- src/App.svelte
+++ src/App.svelte
@@ -1,97 +1,29 @@
-<script lang="ts">
-  import { writable } from 'svelte/store';
-  import dagre from '@dagrejs/dagre';
-  import {
-    SvelteFlow,
-    Background,
-    Position,
-    ConnectionLineType,
-    Panel,
-    type Node,
-    type Edge
-  } from '@xyflow/svelte';
+<script>
+  import {Svelvet, ThemeToggle} from 'svelvet';
+ 
+  import PlanetsInFilms from './lib/tables/Planets_in_films.svelte';
+ 
+  import Films from './lib/tables/Films.svelte';
 
-  import '@xyflow/svelte/dist/style.css';
 
-  import { initialNodes, initialEdges } from './nodes-and-edges';
-
-  const dagreGraph = new dagre.graphlib.Graph();
-  dagreGraph.setDefaultEdgeLabel(() => ({}));
-
-  const nodeWidth = 172;
-  const nodeHeight = 36;
-
-  /**
-   * Calculates the layout positions for the given nodes and edges using the dagre library.
-   * @param {Node[]} nodes - The array of nodes.
-   * @param {Edge[]} edges - The array of edges.
-   * @param {string} direction - The direction of the layout (default: 'TB').
-   * @returns {Object} - An object containing the layouted nodes and edges.
-   */
-  function getLayoutedElements(nodes: any[], edges: any[], direction = 'TB') {
-    const isHorizontal = direction === 'LR';
-    dagreGraph.setGraph({ rankdir: direction });
-
-    nodes.forEach((node) => {
-      dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
-    });
-
-    edges.forEach((edge) => {
-      dagreGraph.setEdge(edge.source, edge.target);
-    });
-
-    dagre.layout(dagreGraph);
-
-    nodes.forEach((node: { id: string | dagre.Label; targetPosition: Position; sourcePosition: Position; position: { x: number; y: number; }; }) => {
-      const nodeWithPosition = dagreGraph.node(node.id);
-      node.targetPosition = isHorizontal ? Position.Left : Position.Top;
-      node.sourcePosition = isHorizontal ? Position.Right : Position.Bottom;
-
-      // We are shifting the dagre node position (anchor=center center) to the top left
-      // so it matches the React Flow node anchor point (top left).
-      node.position = {
-        x: nodeWithPosition.x - nodeWidth / 2,
-        y: nodeWithPosition.y - nodeHeight / 2
-      };
-    });
-
-    return { nodes, edges };
-  }
-
-  const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
-    initialNodes,
-    initialEdges
-  );
-
-  const nodes = writable<Node[]>(layoutedNodes);
-  const edges = writable<Edge[]>(layoutedEdges);
-
-  /**
-   * Performs layout for the elements based on the specified direction.
-   * @param {string} direction - The direction of the layout.
-   */
-  function onLayout(direction: string) {
-    const layoutedElements = getLayoutedElements($nodes, $edges, direction);
-
-    $nodes = layoutedElements.nodes;
-    $edges = layoutedElements.edges;
-    // nodes.set(layoutedElements.nodes);
-    // edges.set(layoutedElements.edges);
-  }
 </script>
+<body>
+  <Svelvet fitView controls minimap>
+    <PlanetsInFilms/>
+    <Films/>
+    <ThemeToggle main='dark' alt='light' slot='toggle'/>
+  </Svelvet>
+</body>
 
-<div style="height:100vh;">
-  <SvelteFlow
-    {nodes}
-    {edges}
-    fitView
-    connectionLineType={ConnectionLineType.SmoothStep}
-    defaultEdgeOptions={{ type: 'smoothstep', animated: true }}
-  >
-    <Panel position="top-right">
-      <button on:click={() => onLayout('TB')}>vertical layout</button>
-      <button on:click={() => onLayout('LR')}>horizontal layout</button>
-    </Panel>
-    <Background />
-  </SvelteFlow>
-</div>
+<style>
+  body {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: gray;
+		width: 100vw;
+		height: 100vh;
+		padding: 0;
+		margin: 0;
+	}
+</style>
