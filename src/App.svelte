dc271c67740bc0943f91bfd26e3a754c45b1ef37
caerbannogwhite <massimo.meneghello93@gmail.com>
2024-01-27 22:28:42 +0000

    Codemirror added.

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,33 @@
 <script>
-  import {Svelvet, ThemeToggle} from 'svelvet';
- 
-  import PlanetsInFilms from './lib/tables/Planets_in_films.svelte';
- 
-  import Films from './lib/tables/Films.svelte';
+  // import { Svelvet, ThemeToggle } from "svelvet";
+  // import PlanetsInFilms from "./lib/tables/Planets_in_films.svelte";
+  // import Films from "./lib/tables/Films.svelte";
 
+  import CodeMirror from "svelte-codemirror-editor";
+  import { python } from "@codemirror/lang-python";
 
+  let value = "";
 </script>
-<body>
+
+<CodeMirror bind:value lang={python()} />
+
+<!-- <body>
   <Svelvet fitView controls minimap>
-    <PlanetsInFilms/>
-    <Films/>
-    <ThemeToggle main='dark' alt='light' slot='toggle'/>
+    <PlanetsInFilms />
+    <Films />
+    <ThemeToggle main="dark" alt="light" slot="toggle" />
   </Svelvet>
 </body>
 
 <style>
   body {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		background-color: gray;
-		width: 100vw;
-		height: 100vh;
-		padding: 0;
-		margin: 0;
-	}
-</style>
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: gray;
+    width: 100vw;
+    height: 100vh;
+    padding: 0;
+    margin: 0;
+  }
+</style> -->
